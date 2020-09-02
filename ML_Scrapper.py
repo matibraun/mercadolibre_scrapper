157bd6672d18702caf84cbb8f7bc5e63e0c23d69
Mati Braun <matibraun@gmail.com>
2020-09-02 19:25:48 -0300

    id de la info ok

--- ML_Scrapper.py
+++ ML_Scrapper.py
@@ -15,4 +15,18 @@ page_num = 1
 
 soup = get_url(url, page_num)
 
-print (soup)
+for item in soup.find_all('li', attrs={'class': 'ui-search-layout__item'}):
+    geographic_area = item.find('span', class_="ui-search-item__group__element ui-search-item__location")
+    print (geographic_area.text.strip())
+
+    price_symbol = item.find('span', class_="price-tag-symbol")
+    print (price_symbol.text.strip())
+
+    price = item.find('span', class_="price-tag-fraction")
+    print (price.text.strip())
+
+    surface = item.find('li', class_="ui-search-card-attributes__attribute")
+    print (surface.text.strip())
+
+    link = item.find('a', class_= 'ui-search-link')['href']
+    print (link)
