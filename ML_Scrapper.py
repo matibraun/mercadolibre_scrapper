0ea89b9cb1bee2628e71b397d8e34192f8eef8b8
Mati Braun <matibraun@gmail.com>
2020-09-03 18:40:36 -0300

    releva toda la info ok

--- ML_Scrapper.py
+++ ML_Scrapper.py
@@ -10,21 +10,46 @@ create_database()
 url = 'https://inmuebles.mercadolibre.com.ar/terrenos-lotes/venta/bsas-gba-norte/escobar/'
 
 page_num = 1
+index = 0
 
 soup = get_url(url, page_num)
 
 for item in soup.find_all('li', attrs={'class': 'ui-search-layout__item'}):
+
+    index += 1
+    print (index)
+
     geographic_area = item.find('span', class_="ui-search-item__group__element ui-search-item__location")
-    print (geographic_area.text.strip())
+    if geographic_area != None:
+        print (geographic_area.text.strip())
+    else:
+        geographic_area = 'N/A'
+        print (geographic_area)
 
     price_symbol = item.find('span', class_="price-tag-symbol")
-    print (price_symbol.text.strip())
+    if price_symbol != None:
+        print (price_symbol.text.strip())
+    else:
+        price_symbol = 'N/A'
+        print (price_symbol)
 
     price = item.find('span', class_="price-tag-fraction")
-    print (price.text.strip())
+    if price != None:
+        print (price.text.strip())
+    else:
+        price = 'N/A'
+        print (price)
 
     surface = item.find('li', class_="ui-search-card-attributes__attribute")
-    print (surface.text.strip())
+    if surface != None:
+        print (surface.text.strip())
+    else:
+        surface = 'N/A'
+        print (surface)
 
-    link = item.find('a', class_= 'ui-search-link')['href']
-    print (link)
+    link = item.find('a', class_= 'ui-search-link')['href'],
+    if link != None:
+        print (link[0])
+    else:
+        link = 'N/A'
+        print (link)
