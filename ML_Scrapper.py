53ada30cca88a3ce824093058f9d07bf3bb4373a
Mati Braun <matibraun@gmail.com>
2020-09-04 15:42:31 -0300

    Funciona perfecto. Proximo paso revisar formatos en los que sube la info.

--- ML_Scrapper.py
+++ ML_Scrapper.py
@@ -1,6 +1,7 @@
 from create_database import create_database
 from web_retriever import get_url
 import sqlite3
+import re
 
 print ("\n ----------------------------")
 print ("| Welcome to Mati's Scrapper |")
@@ -38,14 +39,38 @@ while page_num != 0: #itera pagina por pagina
         price = item.find('span', class_="price-tag-fraction")
         if price != None:
             price = price.text.strip()
+            price = int(price.replace('.', ''))
         else:
             price = 'N/A'
 
-        surface = item.find('li', class_="ui-search-card-attributes__attribute")
-        if surface != None:
-            surface = surface.text.strip()
+        surface_description = item.find('li', class_="ui-search-card-attributes__attribute")
+        if surface_description != None:
+            surface_description = surface_description.text.strip()
         else:
-            surface = 'N/A'
+            surface_description = 'N/A'
+
+        if 'm²' in surface_description:
+            surface_symbol = 'm2'
+        elif 'ha' in surface_description:
+            surface_symbol = 'ha'
+        else:
+            surface_symbol = 'N/A'
+
+        surface = surface_description.replace(',','')
+        surface = (re.findall('\d+', surface))
+
+        if len(surface) == 1:
+            surface_total = int(surface[0])
+            surface_from = 0
+            surface_to = 0
+        elif len(surface) == 2:
+            surface_total = 0
+            surface_from = int(surface[0])
+            surface_to = int(surface[1])
+        else:
+            surface_total = 'N/A'
+            surface_from = 'N/A'
+            surface_to = 'N/A'
 
 
         link = item.find('a', class_= 'ui-search-link')['href'],
@@ -55,23 +80,24 @@ while page_num != 0: #itera pagina por pagina
             link = 'N/A'
 
 #Appendea la data al dict info
-        info[index] = [index, geographic_area, price_symbol, price, surface, link]
+        info[index] = [index, geographic_area, price_symbol, price, surface_description, surface_symbol, surface_from, surface_to, surface_total, link]
 
 
     if len(items) != 0: #para terminar o no el loop por pagina
         page_num = page_num + len(items)
+        page_num = 0
     else:
         page_num = 0
 
-print ('Loading info to database...\n')
+print ('Loading info into database...\n')
 
 ml_scrapper = sqlite3.connect('ml_scrapper.db')
 cursor = ml_scrapper.cursor()
 
 for item in info:
 
-    ingreso = [(info[item][0], info[item][1], info[item][2], info[item][3], info[item][4], info[item][5])]
-    cursor.executemany("INSERT INTO ESCOBAR VALUES (?, ?, ?, ?, ?, ?)", ingreso)
+    ingreso = [(info[item][0], info[item][1], info[item][2], info[item][3], info[item][4], info[item][5], info[item][6], info[item][7], info[item][8], info[item][9])]
+    cursor.executemany("INSERT INTO ESCOBAR VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", ingreso)
 
 ml_scrapper.commit()
 ml_scrapper.close()
